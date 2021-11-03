04286182fd75faa7588e15dfe134caa4a1e4da98
Kin Yuunan <yuunan.kin@gmail.com>
2021-11-04 08:32:08 +0900

    Auth screen part 1: basic markups

---
+++ lib/components/auth-form.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm({Key? key}) : super(key: key);
+
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+          margin: EdgeInsets.all(20),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Form(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Username'),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: 'Password'),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text('Login')),
+                    TextButton(onPressed: () {}, child: Text('Create Account'))
+                  ],
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}
