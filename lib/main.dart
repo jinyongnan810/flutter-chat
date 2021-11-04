c5126859d9deb72c97b5f861cd093f7b7d5f7c0a
Kin Yuunan <yuunan.kin@gmail.com>
2021-11-05 08:30:23 +0900

    setting app themes

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,22 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  final elevatedButtonStyle = ElevatedButton.styleFrom(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)));
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chat',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        backgroundColor: Colors.pink,
+        accentColor: Colors.deepPurple,
+        // imply that accent color is dark, so the contrast text will be white
+        accentColorBrightness: Brightness.dark,
+        // setting elevated button theme
+        elevatedButtonTheme:
+            ElevatedButtonThemeData(style: elevatedButtonStyle),
       ),
       home: AuthScreen(),
     );
